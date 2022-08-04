699da6fa0f6607cf014afef5d0145fe2701d1b35
Hugo Lachkar <hugo1.lachkar@epitech.eu>
2022-08-04 19:25:39 +0200

    feat(frontend): add basic structure

---
+++ packages/frontend/src/components/Header.tsx
@@ -0,0 +1,14 @@
+
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Header = () => (
+  <Container>
+    <span>Logo</span>
+    <button>Connect wallet</button>
+  </Container>
+);
