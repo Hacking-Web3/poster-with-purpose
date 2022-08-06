e4211be6f19736a3a805f12bbbd15346b40f96c8
Igor Line <igor@line.ee>
2022-08-06 10:35:49 +0200

    feat(contracts): add lazy mint logic for zora

--- packages/contracts/contracts/LazyMethod.sol
+++ packages/contracts/contracts/LazyMethod.sol
@@ -5,128 +5,93 @@ import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "./IZoraNFTCreator.sol";
 
-contract LazyMethod {
-    uint256 private prints = 0;
+contract PostersWithPurpose {
+    string private constant name = "PostersWithPurpose";
+    string private constant version = "1.0";
     uint256 private editionNum = 0;
 
-    string name = "Lazy Method";
-    string version = "1.0";
-
-    struct PrintParams {
-        uint256 prints;
-        address signer;
-    }
-
-    struct NFTInfo {
+    struct NftDetails {
         address creator;
         string editName;
         uint16 royaltyBPS;
         address defaultAdmin;
         IERC721Drop.SalesConfiguration saleConfig;
-        string description;
-        string animationURI;
-        string imageURI;
     }
 
-    address ZoraNFTCreatorAddress;
-    IZoraNFTCreator NFTCreator;
+    address public ZoraNFTCreatorAddress;
+    IZoraNFTCreator private NFTCreator;
 
     constructor(address _ZoraNFTCreatorAddress) {
         ZoraNFTCreatorAddress = _ZoraNFTCreatorAddress;
         NFTCreator = IZoraNFTCreator(ZoraNFTCreatorAddress);
     }
 
-    bytes32 constant UPDATE_PRINTS_TYPEHASH = keccak256(
-        "UpdatePrints(uint256 prints,address signer)"
-    );
+    string private constant SALES_CONFIGURATION_SIG = "SalesConfiguration(uint104 publicSalePrice,uint32 maxSalePurchasePerAddress,uint64 publicSaleStart,uint64 publicSaleEnd,uint64 presaleStart,uint64 presaleEnd,bytes32 presaleMerkleRoot)";
+    string private constant CREATE_EDITION_SIG = "CreateEdition(address creator,string editName,uint16 royaltyBPS,address defaultAdmin,SalesConfiguration saleConfig)";
 
-    bytes32 constant CREATE_COLLECTION_TYPEHASH = keccak256(
-        "CreateCollection(address creator,string editName,uint64 editionSize,uint16 royaltyBPS,address payable fundsRecipient,address defaultAdmin,IERC721Drop.SalesConfiguration saleConfig,string description,string animationURI,string imageURI)"
-    );
+    bytes32 private domainSeparator = keccak256(abi.encode(
+        keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
+        keccak256(bytes(name)),
+        keccak256(bytes(version)),
+        block.chainid,
+        address(this)
+    ));
 
-    bytes32 public domainSeparator = keccak256(abi.encode(
-            keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
-            keccak256(bytes(name)),
-            keccak256(bytes(version)),
-            block.chainid,
-            address(this)
-        ));
-
-    function hashPrintsUpdate(bytes32 domainSeparator, PrintParams calldata printParams) private pure returns (bytes32 hash) {
+    function hashCreateEdition(bytes32 domainSeparator, NftDetails calldata nftDetails) private pure returns (bytes32 hash) {
         hash = keccak256(abi.encodePacked(
             "\x19\x01",
             domainSeparator,
             keccak256(
                 abi.encode(
-                    UPDATE_PRINTS_TYPEHASH,
-                    printParams.prints,
-                    printParams.signer
+                    keccak256(abi.encodePacked(CREATE_EDITION_SIG, SALES_CONFIGURATION_SIG)),
+                    nftDetails.creator,
+                    keccak256(bytes(nftDetails.editName)),
+                    nftDetails.royaltyBPS,
+                    nftDetails.defaultAdmin,
+                    keccak256(
+                        abi.encode(
+                            keccak256(bytes(SALES_CONFIGURATION_SIG)),
+                            nftDetails.saleConfig.publicSalePrice,
+                            nftDetails.saleConfig.maxSalePurchasePerAddress,
+                            nftDetails.saleConfig.publicSaleStart,
+                            nftDetails.saleConfig.publicSaleEnd,
+                            nftDetails.saleConfig.presaleStart,
+                            nftDetails.saleConfig.presaleEnd,
+                            nftDetails.saleConfig.presaleMerkleRoot
+                        )
+                    )
                 )
             )
         ));
     }
 
-    function updatePrints(PrintParams calldata printParams, bytes calldata signature) external {
+    function createEdition(NftDetails calldata nftDetails, bytes calldata signature) external returns (address) {
         require(
             ECDSA.recover(
-                hashPrintsUpdate(domainSeparator, printParams),
+                hashCreateEdition(domainSeparator, nftDetails),
                 signature
-            ) == printParams.signer,
+            ) == nftDetails.creator,
             "signature does not match"
         );
-        prints = printParams.prints;
-    }
 
-    function getPrint() public view returns (uint256) {
-        return prints;
-    }
+        editionNum++;
 
-    function hashCreateEdition(bytes32 domainSeparator, NFTInfo calldata nftInfo) private pure returns (bytes32 hash) {
-        hash = keccak256(abi.encodePacked(
-            "\x19\x01",
-            domainSeparator,
-            keccak256(
-                abi.encode(
-                    CREATE_COLLECTION_TYPEHASH,
-                    nftInfo.creator,
-                    nftInfo.editName,
-                    nftInfo.royaltyBPS,
-                    nftInfo.defaultAdmin,
-                    nftInfo.saleConfig,
-                    nftInfo.description,
-                    nftInfo.animationURI
-                )
-            )
-        ));
+        return NFTCreator.createEdition(
+            "edition 1",
+            getSymbol(),
+            100,
+            100,
+            payable(nftDetails.defaultAdmin),
+            nftDetails.defaultAdmin,
+            nftDetails.saleConfig,
+            "",
+            "",
+            ""
+        );
     }
 
     function getSymbol() private returns (string memory){
         string memory baseSymbol = "PWP";
         return string(abi.encodePacked(baseSymbol, Strings.toString(editionNum)));
-    }
-
-    function createEdition(
-        NFTInfo calldata nftInfo, bytes calldata signature) external returns (address) {
-        
-        require(
-            ECDSA.recover(
-                hashCreateEdition(domainSeparator, nftInfo),
-                signature
-            ) == nftInfo.creator,
-            "signature does not match"
-        );
-
-        return NFTCreator.createEdition(
-            "123",
-            getSymbol(),
-            100,
-            100,
-            payable(nftInfo.defaultAdmin),
-            nftInfo.defaultAdmin,
-            nftInfo.saleConfig,
-            "",
-            "",
-            ""
-        );
     }
 }
